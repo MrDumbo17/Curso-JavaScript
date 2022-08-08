189912670cc4540e3f3d89070c42939dada1a231
Mister Dumbo <110036209+MrDumbo17@users.noreply.github.com>
2022-08-08 13:16:34 -0300

    att de erros

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,22 +1,21 @@
 var valores = []
 let resp = document.querySelector('div#res')
+let num = document.querySelector('input#num')
+let list = document.querySelector('section#tabnum')   
 
-function add() {
-
-    let num = document.querySelector('input#num')
-    let list = document.querySelector('section#tabnum')    
+function add() { 
 
     if(num.length == 0) {
         window.alert('Digite um Número')
     }else{
         let valor = Number(num.value)
 
-        if(valores.indexOf(valor) != -1 || valor < 1 || valor > 100) {
+        if(valores.indexOf(valor) != -1 || valor < 1 || valor > 101) {
 
             window.alert('Valor invalido ou ja existente na lista')
         } else {
             
-            valores.push(valor)
+            valores.push(Number(valor))
            
             let item = document.createElement('option')
             item.text = `Valor ${valor} adicionado.`
@@ -25,6 +24,7 @@ function add() {
 
         }
         num.value = ''
+        num.focus()
         resp.innerHTML = ''
     }
 
@@ -42,14 +42,32 @@ function soma() {
     return soma
 }
 function fim() { /* quantidade , o maior numero, o menor, soma , media */
-    let tamanho = Number(valores.length)
-    valores.sort()
+    let tamanho = valores.length
+    if(tamanho == 0){
+
+        window.alert('Adicione Valores!')
+        num.focus()
+
+    }else {
+        
+        var menor = valores[0]
+        var maior = valores[0]
+
+        for(var i in valores){
+
+            if(valores[i] < menor)
+                menor = valores[i]
+            else if(valores[i] > maior)
+                maior = valores[i]
+        }
     
-    resp.innerHTML += `<p>Ao todo temos ${tamanho} numeros cadastrados.</p>`
-    resp.innerHTML += `<p>O maior valor informado foi: ${valores[--tamanho]}</p>`
-    resp.innerHTML += `<p>O menor valor informado foi: ${valores[0]}</p>`
-    resp.innerHTML += `<p>A soma dos valores é : ${soma()}</p>`
-    resp.innerHTML += `<p>A média dos valores é: ${soma()/++tamanho}</p>`
+       resp.innerHTML += `<p>Ao todo temos ${tamanho} numeros cadastrados.</p>`
+       resp.innerHTML += `<p>O maior valor informado foi: ${maior}</p>`
+       resp.innerHTML += `<p>O menor valor informado foi: ${menor}</p>`
+       resp.innerHTML += `<p>A soma dos valores é : ${soma()}</p>`
+       resp.innerHTML += `<p>A média dos valores é: ${soma()/tamanho}</p>`
+       resp.innerHTML += valores
     
+    }
     
 }
